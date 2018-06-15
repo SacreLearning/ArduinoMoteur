d634fd95a0f23191c6141c02d509413e4ac7f8e6
RomainDUGARIN <33719373+Lainkulte@users.noreply.github.com>
2018-06-15 16:05:07 +0200

    Correction

--- speedSensor/classTest.h
+++ speedSensor/classTest.h
@@ -6,33 +6,33 @@
 
 class classTest {
     int counter = 0;
-    char tab [5][5];
+    char tab [6][6];
 
     //LINE 0
-    char [0][0] = 'A';
-    char [0][1] = 'D';
-    char [0][2] = 'G';
-    char [0][4] = 'I';
-    char [0][2] = 'M';
+    tab [0][0] = 'A';
+    tab [0][1] = 'D';
+    tab [0][2] = 'G';
+    tab [0][4] = 'I';
+    tab [0][2] = 'M';
 
     //LINE 1
-    char [1][2] = 'H';
-    char [1][4] = 'J';
-    char [1][5] = 'N';
+    tab [1][2] = 'H';
+    tab [1][4] = 'J';
+    tab [1][5] = 'N';
 
     //LINE 3
-    char [3][0] = 'B';
-    char [3][1] = 'E';
-    char [3][5] = 'O';
+    tab [3][0] = 'B';
+    tab [3][1] = 'E';
+    tab [3][5] = 'O';
 
     //LINE 4
-    char [4][1] = 'F';
-    char [4][5] = 'D';
+    tab [4][1] = 'F';
+    tab [4][5] = 'D';
 
     //LINE 5
-    char [5][0] = 'C';
-    char [5][4] = 'L';
-    char [5][5] = 'P';
+    tab [5][0] = 'C';
+    tab [5][4] = 'L';
+    tab [5][5] = 'P';
 
     int direction = -1;
 
